48ef05ebb219eb546a8f2b8db427355c0b1c72f7
komazsofi <komazsofi@gmail.com>
2019-11-09 16:19:22 +0100

    pre-process

--- Proc_field/connect_lidar_tofield.R
+++ Proc_field/connect_lidar_tofield.R
@@ -11,21 +11,22 @@ library(ggplot2)
 library(scales)
 library(gridExtra)
 
-workingdir="C:/Koma/Sync/_Amsterdam/11_AndrasProject/Analysis/field_measured_data/"
+workingdir="C:/Koma/Sync/_Amsterdam/11_AndrasProject/Analysis/2019Nov/"
 setwd(workingdir)
 
 ## Import
 balaton <- read_excel("quadrat_forR.xlsx",sheet = 2)
-balaton_lidar=read.csv("C:/Koma/Sync/_Amsterdam/11_AndrasProject/Analysis/lidar_anal/balaton_lidar.csv")
+balaton_lidar=read.csv("balaton_lidar.csv")
 
 tisza <- read_excel("quadrat_forR.xlsx",sheet = 3)
-tisza_lidar=read.csv("C:/Koma/Sync/_Amsterdam/11_AndrasProject/Analysis/lidar_anal/tisza_leafoff_lidar.csv")
+tisza_lidar=read.csv("tisza_leafoff_lidar.csv")
+tiszaon_lidar=read.csv("tisza_leafon_lidar.csv")
 
 ferto_1 <- read_excel("quadrat_forR.xlsx",sheet = 4)
-ferto_lidar=read.csv("C:/Koma/Sync/_Amsterdam/11_AndrasProject/Analysis/lidar_anal/ferto_lidar.csv")
+ferto_lidar=read.csv("ferto_lidar.csv")
 ferto_2 <- read_excel("quadrat_forR.xlsx",sheet = 5)
 
-## Merging
+## connact quadrat str. to lidar
 lidar_balaton_merged=merge(balaton,balaton_lidar, by.x=c('point_ID','point_name'), by.y=c('point_ID','point_name'))
 lidar_balaton_merged_cleaned=subset(lidar_balaton_merged,select=c(1,2,7:25,28:87,93:116,133:138))
 
@@ -47,3 +48,16 @@ lidar_tisza_merged_cleaned$lake<- "tisza"
 all_merged=rbind(lidar_balaton_merged_cleaned,lidar_ferto1_merged_cleaned,lidar_tisza_merged_cleaned)
 
 write.csv(all_merged,"lidar_and_field_merged.csv")
+
+## Pole based metrics calc
+
+balaton_lidar$lake <- "balaton"
+tisza_lidar$lake <- "tisza"
+tiszaon_lidar$lake <- "tisza"
+ferto_lidar$lake <- "ferto"
+
+balaton_lidar$season <- "leafoff"
+tisza_lidar$season <- "leafoff"
+tiszaon_lidar$season <- "leafon"
+ferto_lidar$season <- "leafoff"
+
---
+++ Proc_field/get_vegstr_variables.R
@@ -0,0 +1,101 @@
+"
+@author: Zsofia Koma, UvA
+Aim: process wetland OTKA project data step : analyse field data
+"
+
+library(readxl)
+library(data.table)
+
+workingdir="C:/Koma/Sync/_Amsterdam/11_AndrasProject/Analysis/2019Nov/"
+setwd(workingdir)
+
+data <- read_excel("quadrat_forR.xlsx",sheet = 1)
+
+# veg height
+data$veg_height_m=data$veg_height/100
+
+# LAI
+data$sum_leaf_weight=rowSums(data[,c(44,50,53,57,62,66,70,74,78,82)])
+
+# FHD
+data$fhd_pole<-NA
+fhd_pole=subset(data,select=c(12:21))
+
+for (i in seq(1,35)) {
+  print(i)
+  
+  v=as.numeric(fhd_pole[i,])
+  
+  p<- table(v)
+  p <- p/sum(p)
+  entropy=sum(-p*log(p))
+  
+  data$fhd_pole[i] <- entropy
+  
+}
+
+data$fhd_bio<-NA
+fhd_bio=subset(data,select=c(47,51,55,59,63,67,71,75,79))
+
+for (i in seq(1,35)) {
+  print(i)
+  
+  v=as.numeric(fhd_bio[i,])
+  
+  p<- table(v)
+  p <- p/sum(p)
+  entropy=sum(-p*log(p))
+  
+  data$fhd_bio[i] <- entropy
+  
+}
+
+# Rao-quadratic FHD
+
+data$fhd_pole_rao<-NA
+fhd_pole=subset(data,select=c(12:21))
+
+for (i in seq(1,35)) {
+  print(i)
+  
+  v=as.numeric(fhd_pole[i,])
+  p <- v/sum(v)
+  
+  h <- as.matrix(seq(0.5,5,0.5))
+  D=dist(h)
+  D <- as.matrix(D)
+  
+  quadratic_ent=c(crossprod(p, D %*% p)) / 2
+  
+  data$fhd_pole_rao[i] <- quadratic_ent
+  
+}
+
+data$fhd_bio_rao<-NA
+fhd_bio=subset(data,select=c(43,47,51,55,59,63,67,71,75,79))
+
+for (i in seq(1,35)) {
+  print(i)
+  
+  v=as.numeric(fhd_bio[i,])
+  p <- v/sum(v)
+  
+  h <- as.matrix(seq(0.5,5,0.5))
+  D=dist(h)
+  D <- as.matrix(D)
+  
+  quadratic_ent=c(crossprod(p, D %*% p)) / 2
+  
+  data$fhd_bio_rao[i] <- quadratic_ent
+  
+}
+
+# export field calibration between point and quadrat
+data_forcalib_field=subset(data,select=c(2,4,5,6,7,28,22,23,24,25,38,88:93))
+names(data_forcalib_field) <- c("location","FID","point_name","point_ID","veg_type_sp","veg_type","lai","gap_fraction","veg_h_pole","sum_pole_contacts","total weight","veg_height_m","sum_leaf_weight","fhd_pole","fhd_bio","fhd_pole_rao","fhd_bio_rao")
+
+write.csv(data_forcalib_field,"data_for_calib_field.csv")
+
+# export quadrat info to lidar
+data_quadtrat_tolidar=subset(data_forcalib_field,select=c(1:6,11,12,13,15,17))
+write.csv(data_quadtrat_tolidar,"data_quadtrat_tolidar.csv")
